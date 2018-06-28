6b8330c45067eea57ddb8d505f1ff35ce2fd4bf0
Silvan <silvan@dev7.ch>
2018-06-28 15:01:46 +0200

    Update nuxt-ssr-example.vue

--- examples/nuxt-ssr-example/nuxt-ssr-example.vue
+++ examples/nuxt-ssr-example/nuxt-ssr-example.vue
@@ -2,10 +2,7 @@
     <!-- You can find this swiper instance object in current component by the "mySwiper"  -->
     <div v-swiper:mySwiper="swiperOption">
         <div class="swiper-wrapper">
-            <div class="swiper-slide" 
-                v-for="banner in banners"
-                :key="banner.id" 
-                >
+            <div class="swiper-slide" v-for="banner in banners" :key="banner.id">
               <img :src="banner">
             </div>
         </div>
