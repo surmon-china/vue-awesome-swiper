1e3e41c552de19a6f0916de5be0b45fbaf36b59b
surmon-china <surmon@foxmail.com>
2020-03-17 15:19:53 +0800

    TODO: release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# https://github.com/mikeal/merge-release/blob/v4.0.7/merge-release-run.js
 PKG_VERSION=$(jq -r '.version' package.json)
 
 standard-version -a --release-as "$PKG_VERSION"
