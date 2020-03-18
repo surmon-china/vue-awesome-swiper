6d8f1422ecbc470a7f05d9998af39268c7a8daab
surmon-china <surmon@foxmail.com>
2020-03-18 11:51:32 +0800

    Format release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,11 +5,9 @@ set -e
 # https://github.com/mikeal/merge-release/blob/v4.0.7/merge-release-run.js
 PKG_VERSION=$(jq -r '.version' package.json)
 
-standard-version -a --release-as "$PKG_VERSION"
-git push --follow-tags
-
-# git fetch origin v"$PKG_VERSION" || {
-  # yarn global add standard-version
-  # standard-version -a --release-as "$PKG_VERSION"
+git fetch origin v"$PKG_VERSION" || {
+  type standard-version || npm i -g standard-version
+  standard-version -a --release-as "$PKG_VERSION"
+  # TODO: test
   # git push --follow-tags origin "$GH_BRANCH"
-# }
+}
