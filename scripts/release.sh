62a791aecb3e40abd0960b8ad3d81e7506af5829
surmon-china <surmon@foxmail.com>
2020-03-29 22:06:16 +0800

    4.1.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,6 @@ PKG_VERSION=$(jq -r '.version' package.json)
 
 git fetch origin v"$PKG_VERSION" || {
   type standard-version || npm i -g standard-version
-  git add *
   git commit -am "$PKG_VERSION"
   standard-version --skip.changelog --skip.commit -a --release-as "$PKG_VERSION"
   git push --follow-tags origin master
